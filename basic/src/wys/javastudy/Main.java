eed33aa9a92770ecb82d9b2646d4c8098fc4fea8
WYS <2719464332@qq.com>
2020-05-19 20:52:15 +0800

    “添加Java table标签测试"

---
+++ basic/src/wys/javastudy/Main.java
@@ -0,0 +1,8 @@
+package wys.javastudy;
+
+public class Main{
+    public static void main(String args[]){
+        TestLabel testlabel = new TestLabel();
+        testlabel.label();
+    }
+}
---
+++ basic/src/wys/javastudy/TestLabel.java
@@ -0,0 +1,36 @@
+package wys.javastudy;
+
+import java.io.*;
+public class TestLabel{
+    public TestLabel(){
+
+    }
+
+    public void label(){
+        int i = 0;
+
+        outer: //Can't have statements here
+        for(;true;){
+            inner://Can't have statements here
+            for(;i < 10; i++){
+                System.out.println("i : "+i);
+                if(i == 2){
+                    System.out.println("continue");
+                    continue;
+                }
+                if(i == 3){
+                    System.out.println("break");
+                    break;
+                }
+                if( i == 7){
+                    System.out.println("continue outer");
+                    break outer;
+                }
+                if( i == 8){
+                    System.out.println("continue inner");
+                    continue inner;
+                }
+            }
+        }
+    }
+}
